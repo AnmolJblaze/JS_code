fb2dd83f364b133f9ec012a843579dbae18f92fd
Anmol <anmoljoshi27feb@gmail.com>
2024-01-05 08:44:43 +0000

    dataTypes

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat this code as per the newer version
+
+//alert(3+3);
+// we are using nodejs and not the browser
+
+//number
+//bigInt
+// String   - - - prefer using Double quotes
+
+//Boolean
+//Null stand alone value
+//undefined => value is defined and not intialized 
+
+//symbol - to deefine an indiviual component or uniqueness 
+
+//Object
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); //undefined
+
---
+++ basics/variables.js
@@ -0,0 +1,17 @@
+const account_id = 911;
+let account_Email = "anmol@google.com" 
+var account_password = "3210"
+account_city  = 'Delhi' // naming of varibales like this to be avoided.
+
+
+//account_id = 210;
+account_Email = "anmol1@gmail.com"
+account_password = "5113"
+account_city = "Noida"
+
+/*
+Do not use var keyword to define variables 
+because of issues in blaock and functional scope 
+*/
+
+console.table([account_id,account_Email,account_password,account_city])
