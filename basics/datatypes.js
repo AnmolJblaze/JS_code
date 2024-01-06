f570038604b2d5265b800c52c49e4c1360f44a48
Anmol <anmoljoshi27feb@gmail.com>
2024-01-06 16:54:13 +0000

    datatypes summary done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -15,7 +15,30 @@
 
 //Object
 
-console.log(typeof null); // object
+//console.log(typeof null); // object
 
-console.log(typeof undefined); //undefined
+//console.log(typeof undefined); //undefined
 
+// Primitive datatypes are stored in stacks  -- Number, String,null,undefined,symbol,BigInt,Boolean
+// Non Primitive are stored in heap  -- functions,objects,arrays
+
+// changes made to primitive datatypes are changes made to the copy, while changes made to the no primitive dataypes 
+// are refernces so making changes on them will chnage the originial value as well
+
+let name_1 = "Anmol";
+let another_name = name_1;
+another_name = "Joshi"
+
+console.table([name_1,another_name]);
+// changes made are passed to the copy rather than changing the original value
+
+var myObj = {
+    name:"anmol",
+    age:"26"
+}
+
+let anotherObj = myObj;
+anotherObj.name = "Joshi";
+
+console.table([myObj,anotherObj]);
+// here the changes made are made to the reference, therefore any change will change the original value
